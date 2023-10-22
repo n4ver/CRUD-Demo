6d7343d79df95cc66d6e5290f953c81fe93e6252
n4ver <nicholaslkchng@gmail.com>
2023-10-22 23:56:08 +0800

    swap to laptop

--- client/src/components/Error.vue
+++ client/src/components/Error.vue
@@ -13,14 +13,14 @@ export default {
     <main>
         <section class="w-full flex items-center justify-center">
             <div class="text-center">
-                <h1 class="text-6xl md:text-9xl font-medium">
+                <h1 class="text-6xl md:text-9xl font-medium  text-gray-900 dark:text-white">
                     {{ errorCode }}
                 </h1>
-                <div class="py-4 text-2xl md:text-3xl">
+                <div class="py-4 text-2xl md:text-3xl text-gray-900 dark:text-white">
                     {{ errorMessage }}
                 </div>
                 <div class="py-4">
-                    <a href="/" class="hover:underline">Back to the Homepage</a>
+                    <a href="/" class="hover:underline text-gray-900 dark:text-white">Back to the Homepage</a>
                 </div>
             </div>
         </section>
